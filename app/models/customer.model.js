de95b9a01c16354c270a3108b4f3007061e7646c
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2020-02-13 13:06:24 +0700

    add customer.models.js

---
+++ app/models/customer.model.js
@@ -0,0 +1,19 @@
+const sql = require("./db")
+
+const Customer = function (customer) {
+    this.email = customer.emaill;
+    this.name = customer.name;
+    this.active = customer.active;
+}
+
+Customer.getAllUsers = result => {
+    sql.query("select * from customers", (err, res) => {
+        if(err){
+            console.log("error: ", err);
+            result(null, err);
+        }
+
+        console.log("Customers: ", res);
+        result(null, res)
+    });
+}
